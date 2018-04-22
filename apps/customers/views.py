014e3a3210659358fc8543a52e453c688907e273
caiovictormc <caiovictor31@live.com>
2018-04-21 21:23:50 -0300

    forgot password front

--- apps/customers/views.py
+++ apps/customers/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class ForgotPassword(TemplateView):
+    template_name = 'auth/forgot-password.html'
--- mes-cloud/urls.py
+++ mes-cloud/urls.py
@@ -17,12 +17,17 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
+from apps.customers import views as customers_views
+
+
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login,
         {'template_name': 'auth/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout,
         {'next_page': '/login'}, name='logout'),
-    url(r'^admin/', admin.site.urls),
+    url(r'^forgot-password/$', customers_views.ForgotPassword.as_view(),
+        name='forgot-password'),
 
     url(r'^', include('apps.dashboard.urls', namespace='dashboard')),
 ]
