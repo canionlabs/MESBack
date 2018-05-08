0895a5ffb3852e79e282acb4433290d63f978319
caiovictormc <caiovictor31@live.com>
2018-05-08 10:32:27 -0300

    Update verbose names - model DefaultModel

--- apps/core/models.py
+++ apps/core/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class DefaultModel(models.Model):
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField('Criado em', auto_now=True)
+    updated_at = models.DateTimeField('Atualizado em'auto_now_add=True)
 
     class Meta:
         abstract = True
